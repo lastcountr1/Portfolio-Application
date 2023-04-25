b9e45f42f6dede13af66aaed4c1bcc762a7977b3
lastcountr <lastcountr@gmail.com>
2023-04-26 02:02:42 +0530

    Adding some required files

---
+++ app/src/main/java/com/example/kidquiz/mainpage.kt
@@ -0,0 +1,11 @@
+package com.example.kidquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mainpage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mainpage)
+    }
+}
